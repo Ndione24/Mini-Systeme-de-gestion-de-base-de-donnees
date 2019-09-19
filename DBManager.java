1a656bfd25e79b38991e474c0bbbfc49101fc16f
Ndione24 <47160424+Ndione24@users.noreply.github.com>
2019-09-19 19:32:32 +0200

    Add files via upload

---
+++ DBManager.java
@@ -0,0 +1,42 @@
+package DataBase;
+
+public class DBManager {
+	
+	/**
+	 * description: qui fera le nécessaire pour l’initialisation d’une instance
+	 * @param
+	 */
+	public static  void init()
+	{
+		
+	}
+	/**
+	 @Description: s'occupe du ménage 
+	 */
+	public static void finish()
+	{
+		
+	}
+	
+	/*
+	 * @param :commande entrée 
+	 */
+	public void processCommand(byte command) 
+	{
+		
+	}
+
+	public static void main(String [] args)
+	{
+		//la création (si besoin) de l’instance de DBManage
+		
+		init();
+		
+		do
+		{
+			
+		} while(args[0]!="exit");
+	}
+}
+
+
