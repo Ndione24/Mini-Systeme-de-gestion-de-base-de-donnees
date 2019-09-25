ec697047041f2a63f747a6b1032fd2ae3407f440
zerhounimed <33357555+zerhounimed@users.noreply.github.com>
2019-09-26 00:38:01 +0200

    Update DBDef.java

--- DBDef.java
+++ DBDef.java
@@ -2,13 +2,27 @@ package DataBase;
 import java.util.*;
 
 public class DBDef {
-	private int compteur;
-	private ArrayList<RelDef> al;
 	
-	public DBDef(ArrayList<RelDef> al, int compteur ) 
+	private int compteur;
+	private ArrayList<RelDef> listeRelDef ;
+	private static DBDef instanceUniqueDBDef ;
+	
+	public DBDef(ArrayList<RelDef> listeRelDef, int compteur ) 
 	{
-		this.al=new ArrayList<RelDef> ();
+		this.listeRelDef=new ArrayList<RelDef> ();
 		this.compteur=compteur;
+		
+	}
+	
+	public void init () {
+	}
+	
+	public void finish () {
+	}
+	
+	public void addRelation (RelDef rel ) {
+		listeRelDef.add(rel) ;
+		compteur++ ;
 		
 	}
 }
