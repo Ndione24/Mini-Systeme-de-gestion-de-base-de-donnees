0cad9ca8e011d5255592e5d05cbd2c7f22de7720
MohamedUrameshi <55348679+MohamedUrameshi@users.noreply.github.com>
2019-09-26 17:34:27 +0200

    Update DBDef.java

--- DBDef.java
+++ DBDef.java
@@ -1,28 +1,58 @@
-package DataBase;
-import java.util.*;
+package projetBdd;
+
+import java.util.ArrayList;
 
 public class DBDef {
-	
+
 	private int compteur;
-	private ArrayList<RelDef> listeRelDef ;
-	private static DBDef instanceUniqueDBDef ;
-	
-	public DBDef(ArrayList<RelDef> listeRelDef, int compteur ) 
-	{
-		this.listeRelDef=new ArrayList<RelDef> ();
-		this.compteur=compteur;
-		
+	private ArrayList<RelDef> listeRelDef;
+	private static DBDef INSTANCE = new DBDef();
+
+	public DBDef(ArrayList<RelDef> listeRelDef, int compteur) {
+		this.listeRelDef = new ArrayList<RelDef>();
+		this.compteur = compteur;
+
 	}
-	
-	public void init () {
+
+	/**
+	 * Constructeur sans argument qui initialise les attributs
+	 */
+	private DBDef() {
+		listeRelDef = new ArrayList<RelDef>();
+		compteur = 0;
 	}
-	
-	public void finish () {
+
+	/**
+	 * <i>L'accès à l'unique Instance Instance de la classe</i>
+	 * 
+	 * @return <i>Unique instance</i>
+	 */
+	public static DBDef getINSTANCE() {
+		if (INSTANCE == null) {
+			INSTANCE = new DBDef();
+		}
+		return INSTANCE;
 	}
-	
-	public void addRelation (RelDef rel ) {
-		listeRelDef.add(rel) ;
-		compteur++ ;
-		
+
+	public void init() {
+	}
+
+	public void finish() {
+	}
+
+	/**
+	 * Cette metode ajoute la relation rel dans la liste si elle la contient pas
+	 * d'abord
+	 * 
+	 * @param rel : la nouvelle relation à ajouter
+	 */
+	public void addRelation(RelDef rel) {
+		if (!listeRelDef.contains(rel)) {
+			listeRelDef.add(rel);
+			compteur++;
+		} else {
+			System.out.println("La liste contient déjà cette relation");
+		}
+
 	}
 }
