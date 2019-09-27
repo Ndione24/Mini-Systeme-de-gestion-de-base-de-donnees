988afa1bbca079084dbbbc360d5eef59507fc764
MohamedUrameshi <55348679+MohamedUrameshi@users.noreply.github.com>
2019-09-27 14:38:52 +0200

    Update PageId.java

--- PageId.java
+++ PageId.java
@@ -1,17 +1,53 @@
-package DataBase;
+package projetBdd;
 
 public class PageId {
+	// identifiant du fichier
 	private int fileIdx;
+	// identifiant de la page
 	private int pageIdx;
-	
-	public PageId(int fileIdx,  int pageIdx) {
-		this.fileIdx=fileIdx;
-		this.pageIdx=pageIdx;
+
+	/**
+	 * <Ce constructeur met à jour les attributs
+	 * 
+	 * @param fileIdx <i>Représente l'indentifier du fichier</i>
+	 * @param pageIdx <i>Représente l'indentifier de la page</i>
+	 */
+	public PageId(int fileIdx, int pageIdx) {
+		this.fileIdx = fileIdx;
+		this.pageIdx = pageIdx;
 	}
-	
-	
-	public void getFileId() 
-	{
-		
+
+	/**
+	 * Constructeur sans argument initialise les attributs
+	 */
+	public PageId() {
+		fileIdx = 0;
+		pageIdx = 0;
+	}
+
+//getters et setters
+	public void setFileIdx(int fileIdx) {
+		this.fileIdx = fileIdx;
+	}
+
+	public void setPageIdx(int pageIdx) {
+		this.pageIdx = pageIdx;
+	}
+
+	public int getFileIdx() {
+		return fileIdx;
+	}
+
+	public int getPageIdx() {
+		return pageIdx;
+	}
+
+	/**
+	 * <i>Rédefinition de la méthode toString de la classe Object</i>
+	 * 
+	 * @return <i>L'identifiant du fichier et de la pageId</i>
+	 */
+	public String toString() {
+		return " " + fileIdx + " " + pageIdx;
 	}
 }
