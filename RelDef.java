1d992dc3cd5a41c4c36237f482fb8c216b4487cd
Mohamed Djimé <55348679+MohamedUrameshi@users.noreply.github.com>
2019-10-17 16:40:46 +0200

    Update RelDef.java
    Ajout de nouvelle variables d'après le tp4

--- RelDef.java
+++ RelDef.java
@@ -1,12 +1,22 @@
-package DataBase ;
+package projetBdd;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class RelDef {
 
+	// nomRelation qui represente le nom d'une relation donnees
 	private String nomRelation;
+	// nbColonnes rep le nombre de colonnes de la relation
 	private int nbColonnes;
+	// typesColonnes qui rep les types de differents colonnes
 	private List<String> typesColonnes;
+	// fileIdx, qui correspond à l’indice du fichier disque qui stocke la relation
+	private int fileIdx;
+	// recordSize qui représente la taille d’un record
+	private int recordSize;
+	// slotCount, qui représente le nombre de cases (slots) sur une page
+	private int slotCount;
 
 	/**
 	 * @description :1er constructeur de la classe
@@ -22,11 +32,38 @@ public class RelDef {
 		this.typesColonnes = typesColonnes;
 	}
 
+	public RelDef() {
+
+		this.nomRelation = "";
+		this.nbColonnes = 0;
+		this.typesColonnes = new ArrayList<String>();
+	}
+
 	public RelDef(String nomRelation, int nbColonnes) {
 
 		this.nomRelation = nomRelation;
 		this.nbColonnes = nbColonnes;
 		this.typesColonnes = new ArrayList<String>();
+	}
+
+	/**
+	 * constructeur definit au niveau tp4
+	 * 
+	 * @param nomRelation
+	 * @param nbColonnes
+	 * @param typesColonnes
+	 * @param fileIdx
+	 * @param recordSize
+	 * @param slotCount
+	 */
+	public RelDef(String nomRelation, int nbColonnes, List<String> typesColonnes, int fileIdx, int recordSize,
+			int slotCount) {
+		this.nomRelation = nomRelation;
+		this.nbColonnes = nbColonnes;
+		this.typesColonnes = typesColonnes;
+		this.fileIdx = fileIdx;
+		this.recordSize = recordSize;
+		this.slotCount = slotCount;
 	}
 
 	public String getNomRelation() {
@@ -51,6 +88,30 @@ public class RelDef {
 
 	public void setTypesColonnes(List<String> typesColonnes) {
 		this.typesColonnes = typesColonnes;
+	}
+
+	public int getFileIdx() {
+		return fileIdx;
+	}
+
+	public void setFileIdx(int fileIdx) {
+		this.fileIdx = fileIdx;
+	}
+
+	public int getRecordSize() {
+		return recordSize;
+	}
+
+	public void setRecordSize(int recordSize) {
+		this.recordSize = recordSize;
+	}
+
+	public int getSlotCount() {
+		return slotCount;
+	}
+
+	public void setSlotCount(int slotCount) {
+		this.slotCount = slotCount;
 	}
 
 }
