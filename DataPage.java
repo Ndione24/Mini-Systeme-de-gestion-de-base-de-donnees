c4de0c77da39fab2d1a1aa5291e8b0d69c3439cd
Ndione24 <47160424+Ndione24@users.noreply.github.com>
2019-11-07 18:10:11 +0100

    Update DataPage.java

--- DataPage.java
+++ DataPage.java
@@ -1,36 +1,71 @@
-package projetBdd;
-//for a page
+package DataBase;
+
+
+import java.util.ArrayList;
+
 public class DataPage {
-//id de la page
-	private int idxDeLaPage;
-//nbre de cases dispo
-	private int freeSlots;
-	
-	public DataPage() {
-		this.idxDeLaPage=0;
-		this.freeSlots=0;
-	}
-	/**
-	 * constructeur 2
-	 */
-	public DataPage(int idxDeLaPage,int freeSlots) {
-		this.idxDeLaPage=idxDeLaPage;
-		this.freeSlots=freeSlots;
-	}
+	//slotCount le nombre de cases sur une page de données. 
+		private int slotCount;
+		
+		PageId page;
+		private int freeSlots;
+		private byte [] cases;
+		
+		
+		public DataPage(PageId page,RelDef reldef){
+			//slotCount associé à une relation
+			
+			this.slotCount=reldef.getSlotCount();
+			//le nombre de Slot libre correspond au nombre de Slot au debut 
+			this.freeSlots=reldef.getSlotCount();
+			this.page=page;
+			
+			
+			
+			//size des tableaux à determininer
+			this.cases= new byte[ slotCount + (slotCount*reldef.getRecordSize())];
+			
+			//on condidere que la premiere case s'agit de la bytemap
+			for(int i=0;i<slotCount;i++) 
+			{
+				this.cases[0]= 0;
+			}
+			
+			
+			}
+				
+		public byte[] getBufferDataPage() 
+		{
+			return this.cases;
+		}
+		
+		public int getSlotCount() {
+			return slotCount;
+		}
+		
+		
+		public int incrementefreeSlots() 
+		{
+			return this.freeSlots++;
+		}
 
-	public int getIdxDeLaPage() {
-		return this.idxDeLaPage;
-	}
+		
+		public int decrementeFreeSlots() 
+		{
+			return this.freeSlots--;
+		}
+		
+		
 
-	public void setIdxDeLaPage(int idx) {
-		this.idxDeLaPage = idx;
-	}
+		public void setSlotCount(int slotCount) {
+			this.slotCount = slotCount;
+		}
 
-	public int getFreeSlots() {
-		return freeSlots;
-	}
+		public int getFreeSlots() {
+			return freeSlots;
+		}
 
-	public void setFreeSlots(int freeSlots) {
-		this.freeSlots = freeSlots;
+		public void setFreeSlots(int freeSlots) {
+			this.freeSlots = freeSlots;
+		}
 	}
-}
