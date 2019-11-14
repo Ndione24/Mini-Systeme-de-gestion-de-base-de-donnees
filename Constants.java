235720b2c8139bb13ff32993154c30640a16afb7
Ndione24 <47160424+Ndione24@users.noreply.github.com>
2019-11-14 16:50:54 +0100

    Update Constants.java

--- Constants.java
+++ Constants.java
@@ -1,10 +1,12 @@
-package projetBdd;
-
 public class Constants {
-  //la taille d'un page
-public static final int PAGESIZE = 4096;
-  //nombre de case =2
-public static final int FRAMECOUNT=2;
-  
+	
+	  //la taille d'un page
+	public static final byte PAGESIZE = 10;
+	  //nombre de case =2
+	public static final int FRAMECOUNT=2;
 
+	public static String CHEMINDB="chemin de votre repertoire DB à mettre ";
+	
+	public static String CHEMINRACINE="";
+	
 }
